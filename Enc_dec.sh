cc90bfa11021097470008a3230c407dabddfd6f6
douchey01Cuppa <93307195+douchey01Cuppa@users.noreply.github.com>
2021-11-05 22:50:08 +0530

    Remove unwanted comments and indentations
    uniformize paramter logic

--- Enc_dec.sh
+++ Enc_dec.sh
@@ -20,18 +20,12 @@
 # NOTE: For now this script relies on defulat AES CFB.. might switch to  "--full-generate-key" later
 
 
-
-
 echo "GPG Encrypt shell script  v1.0"
 
 read -p "path to a File/Directory: " enc_obj
 
-
- 
-
 read -p "1 For Encryption
  2 For  Decryption:  "  choice
-
 
 ####### Encrypt block START
 
@@ -39,7 +33,7 @@ function encrypt {
 
 if [ -f  $1 ]; then
    echo "It's a file.. Encrypting the file..."
-   gpg -c --no-symkey-cache $1 && echo  "Encrypted file stored as $1.gpg" #to prevent storing pass cache in memory
+   gpg -c --no-symkey-cache $1 && echo  "Encrypted file stored as ${1##*/}.gpg" #to prevent storing pass cache in memory
 
 
 elif [ -d $1 ]; then
@@ -66,38 +60,21 @@ if [[ "$1"  == *.tar.gz.gpg ]]; then
     echo "You entered a Directory"
     gpg --no-symkey-cache $1 
     mkdir -p "${1%%.tar.gz.gpg}" && tar -xzf "${1%.gpg}" 2>/dev/null  && echo "Successfully decrypted directoy"
-
     
-    #rm -r "${1%.gpg}" 
-  
-
-
 elif [[ $1  == *.gpg  ]]; then
    echo "It's a file.. Decrypting the file..."
    gpg --no-symkey-cache $1 && echo  "File Succefully decrypted!"
-  
-
 else
-
    echo "Invalid enc_object type detected!!"
 fi
 }
 # Decrypt func Block END -----------------------------
-
-
-
-
 if [ $choice == 1 ]; then 
    encrypt $enc_obj
-
-
+   
 elif [ $choice == 2 ];then 
     decrypt $enc_obj
-
 else 
   echo "invalid choice"    
-
-fi
- 
+fi 
 exit 0 
-# ~/Downloads/fonts.tar.gz.gpg
